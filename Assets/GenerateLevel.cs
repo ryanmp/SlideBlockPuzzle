481bbe5beedd605102c2853e3a214015271e022e
Ryan Phillips <wordsandcolor@gmail.com>
2015-08-31 16:59:52 -0700

    touch input & levels from txt files

--- Assets/GenerateLevel.cs
+++ Assets/GenerateLevel.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+
+
 
 public class GenerateLevel : MonoBehaviour
 {
@@ -8,10 +11,8 @@ public class GenerateLevel : MonoBehaviour
 	// prefabs
 	public GameObject block;
 	public GameObject fixed_block;
-
-
-
-	public int grid_dimensions = 3; // 2n + 1  x 2n + 1 (7x7 in this case)
+	
+	private int grid_dimensions = 0; 
 	public float GRAVITY_MAGNITUDE = 10f;
 
 	public Color[] block_colors; // currently using the WAR color palette from colourlovers.com
@@ -19,35 +20,55 @@ public class GenerateLevel : MonoBehaviour
 	private List<Vector2> block_init_ps = new List<Vector2> () {
 		new Vector2 (1f, 1f),
 		new Vector2 (2f, 1f),
-		new Vector2 (1f, 2f),
-		new Vector2 (-1f, -2f)
+		new Vector2 (1f, 0f),
+		new Vector2 (-1f, 0f)
 	};
 	private List<Vector2> fixed_block_init_ps = new List<Vector2> (){
 		new Vector2 (0f, 0f)
 	};
 
+	private List<Vector3> blocks = new List<Vector3> (); // z component = block type
+
+
 	// Use this for initialization
 	void Start ()
 	{
-		Random.seed = 123;
+		//Random.seed = 123;
 
-		// create init blocks (fixed and moveable)
-		foreach (Vector2 b_position in fixed_block_init_ps) {
-			GameObject clone = Instantiate (fixed_block, b_position, Quaternion.identity) as GameObject;
+		// read in level from file
+		TextAsset l = Resources.Load ("1") as TextAsset;
+		string[] lines = l.text.Split ("\n" [0]);
+		grid_dimensions = lines.Length;
+		int[] block_types = { 0,1,2 };
+		for (int i = 0; i < lines.Length; i++) {
+			for (int j = 0; j < lines[0].Length; j++) {
+				double this_block = char.GetNumericValue (lines [i] [j]);
+				int x = j - grid_dimensions / 2;
+				int y = (i - grid_dimensions / 2) * -1;
+				if (this_block >= 0f) {
+					blocks.Add (new Vector3 (x, y, (float)this_block));
+				}
+			}
 		}
 
-		foreach (Vector2 b_position in block_init_ps) {
-			GameObject clone = Instantiate (block, b_position, Quaternion.identity) as GameObject;
-			clone.GetComponent<SpriteRenderer> ().color = block_colors [Random.Range (0, block_colors.Length)];
+		// create init blocks
+		foreach (Vector3 b_position in blocks) {
+			int z = (int)b_position.z;
+			if (z == 0) { // fixed block type
+				GameObject clone = Instantiate (fixed_block, new Vector2 (b_position.x, b_position.y), Quaternion.identity) as GameObject;
+			} else { //moveable block type
+				GameObject clone = Instantiate (block, new Vector2 (b_position.x, b_position.y), Quaternion.identity) as GameObject;
+				clone.GetComponent<SpriteRenderer> ().color = block_colors [z - 1];
+			}
 		}
 
 		// build level bounding box
 		string[] names = new string[]{"bottom","top","left","right"};
 		Vector3[] positions = new Vector3[]{
-			new Vector3 (0f, -1f * grid_dimensions - 0.5f, 0f),
-			new Vector3 (0f, grid_dimensions + 0.5f, 0f),
-			new Vector3 (-1f * grid_dimensions - 0.5f, 0f, 0f),
-			new Vector3 (grid_dimensions + 0.5f, 0f, 0f)
+			new Vector3 (0f, -1f * grid_dimensions / 2f, 0f),
+			new Vector3 (0f, grid_dimensions / 2f, 0f),
+			new Vector3 (-1f * grid_dimensions / 2f, 0f, 0f),
+			new Vector3 (grid_dimensions / 2f, 0f, 0f)
 		};
 		Vector3[] rotations = new Vector3[]{
 			new Vector3 (0f, 0f, 0f),
@@ -57,7 +78,7 @@ public class GenerateLevel : MonoBehaviour
 		};
 		for (int i = 0; i < names.Length; i++) {
 			GameObject o = new GameObject (names [i]);
-			o.transform.localScale = new Vector3 (grid_dimensions * 2f + 1f, grid_dimensions * 2f + 1f, grid_dimensions * 2f + 1f);
+			o.transform.localScale = new Vector3 (grid_dimensions, grid_dimensions, grid_dimensions);
 			o.transform.parent = gameObject.transform;
 			o.AddComponent<EdgeCollider2D> ();
 			o.transform.position = positions [i];
@@ -65,7 +86,7 @@ public class GenerateLevel : MonoBehaviour
 		}
 
 		Physics2D.gravity = new Vector2 (0f, -GRAVITY_MAGNITUDE);
-		InvokeRepeating ("ChangeGravity", 3f, 3f);
+		//InvokeRepeating ("ChangeGravity", 3f, 3f);
 
 	}
 	
@@ -75,11 +96,18 @@ public class GenerateLevel : MonoBehaviour
 
 	}
 
-	void ChangeGravity ()
+	public void ChangeGravity (Vector2 v)
 	{
+		/*
 		Physics2D.gravity = new Vector2 (
 			RandBinary () * GRAVITY_MAGNITUDE,
 			RandBinary () * GRAVITY_MAGNITUDE
+		);*/
+
+
+		Physics2D.gravity = new Vector2 (
+			v.x * GRAVITY_MAGNITUDE,
+			v.y * GRAVITY_MAGNITUDE
 		);
 	}
 	
